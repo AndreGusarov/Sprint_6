5a8e4ffc967d73232fdde19f2a34b504af65adf0
Andrei Gusarov <andregator@tut.by>
2025-04-08 12:10:53 +0300

    Откат

--- data/urls.py
+++
@@ -1,6 +0,0 @@
-class Url:
-    MAIN_PAGE = "https://qa-scooter.praktikum-services.ru"
-    ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order"
-    YANDEX_DZEN = "https://dzen.ru/"
-    YANDEX_HOME_PAGE = "https://ya.ru"
-    ORDER_STATUS_PAGE = 'https://qa-scooter.praktikum-services.ru/track'
